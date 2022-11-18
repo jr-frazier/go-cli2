40aa55f114ccdf7ee6670cf0a722cfa0a1efda27
jr-frazier <jfrazier@tiggee.com>
2022-11-18 09:41:11 -0500

    updated push script

--- .github/actions/push-to-packagecloud/push.sh
+++ .github/actions/push-to-packagecloud/push.sh
@@ -3,17 +3,19 @@
 set -e
 
 echo "Install dependencies"
-gem install package_cloud
+sudo gem install package_cloud
 
-echo "PACKAGECLOUD_REPO=${PACKAGECLOUD_REPO}"
-echo "PACKAGECLOUD_DISTRIB=${PACKAGECLOUD_DISTRIB}"
-echo "PACKAGE_NAME=${PACKAGE_NAME}"
+echo "Pushing to packagecloud"
 
-UPLOAD_PATH="${PACKAGECLOUD_USERNAME}/${PACKAGECLOUD_REPO}"
-if [ -n "${PACKAGECLOUD_DISTRIB}" ]; then
-    UPLOAD_PATH="${UPLOAD_PATH}/${PACKAGECLOUD_DISTRIB}"
-fi
-
-echo "Deploying package: ${PACKAGE_NAME}"
-echo "To repository: ${UPLOAD_PATH}"
-package_cloud push ${UPLOAD_PATH} ${PACKAGE_NAME}
+#echo "PACKAGECLOUD_REPO=${PACKAGECLOUD_REPO}"
+#echo "PACKAGECLOUD_DISTRIB=${PACKAGECLOUD_DISTRIB}"
+#echo "PACKAGE_NAME=${PACKAGE_NAME}"
+#
+#UPLOAD_PATH="${PACKAGECLOUD_USERNAME}/${PACKAGECLOUD_REPO}"
+#if [ -n "${PACKAGECLOUD_DISTRIB}" ]; then
+#    UPLOAD_PATH="${UPLOAD_PATH}/${PACKAGECLOUD_DISTRIB}"
+#fi
+#
+#echo "Deploying package: ${PACKAGE_NAME}"
+#echo "To repository: ${UPLOAD_PATH}"
+#package_cloud push ${UPLOAD_PATH} ${PACKAGE_NAME}
