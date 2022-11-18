8c336e5d2c2712c2bc8030316b27affe54b1210e
jr-frazier <jfrazier@tiggee.com>
2022-11-18 09:34:53 -0500

    updated push script

--- .github/actions/push-to-packagecloud/push.sh
+++ .github/actions/push-to-packagecloud/push.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Install dependencies"
-sudo gem install package_cloud
+gem install package_cloud
 
 echo "PACKAGECLOUD_REPO=${PACKAGECLOUD_REPO}"
 echo "PACKAGECLOUD_DISTRIB=${PACKAGECLOUD_DISTRIB}"
